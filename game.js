fe6e0b029d1d641eabc2829acdbe46b47251985c
Prems303 <premsagarthogaru@yahoo.com>
2016-06-20 20:32:39 +1200

    syntax errror corrected

--- game.js
+++ game.js
@@ -43,7 +43,7 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-  var dots = document.getElementsByClassName('board')[0].children)
+  var dots = document.getElementsByClassName('board')[0].children
     for (var i = 0; i < dots.length; i++) {
       if (dots[i].classList.contains('blue')) {
         totals.blue++;
